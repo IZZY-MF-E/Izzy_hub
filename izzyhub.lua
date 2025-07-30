1da4c3aa2d2e960713bd8bc54d82971e5e25222a
IZZY-MF-E <gnttfr@gmail.com>
2025-07-30 14:34:09 +0300

    izzyhub.lua

--- izzyhub.lua
+++ izzyhub.lua
@@ -1,8 +1,15 @@
 -- Izzy Hub Loader
 local msg = Instance.new("Message", workspace)
 msg.Text = "Izzy is loading...😉"
-wait(2) -- pause for effect
+wait(4) -- pause for effect
+
+msg.Text = "tiktok @itz_me_iz"
+wait(2)
+
+msg.Text = "here it is💋"
+wait(1.5)
+
 msg:Destroy()
 
 -- Load main script from Pastefy
-loadstring(game:HttpGet("https://pastefy.app/izg1HqVu/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/fhGDSVwV/raw"))()
